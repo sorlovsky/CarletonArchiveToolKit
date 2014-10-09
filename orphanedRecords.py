4009388dbd39e244420b7222fcd10b1a70c00925
simonorlovsky <simon.orlovsky@yahoo.com>
2014-10-09 09:30:02 -0500

    Figured out way to get list of correct entries

--- orphanedRecords.py
+++ orphanedRecords.py
@@ -5,26 +5,29 @@
 #content data set
 file = open("tblCollections_Content.txt")
 
-count=0
 contentFields = []
 for line in file:
     fields = line.split(',')
     contentFields.append(fields)
-    count+=1
-
-print "There were", count, "files in content"
-print contentFields[0][0]
 
 #digital records data set
 file = open("tblDigitalLibrary_DigitalContent.txt")       
 
 digitalFields = []
-count=0
 for line in file:
     fields = line.split(',')
     digitalFields.append(fields);
-    count+=1
 file.close()
 
-print "There were", count, "files in digital content"
+correctList = []
+for i in range(len(digitalFields)):
+    for j in range(len(contentFields)):
+        if digitalFields[i][0] == contentFields[j][0]:
+            correctList.append(digitalFields[i])
+            break
+
+print correctList
+
+# for i in range(len(digitalFields)):
+#     for j in range(len(correctList)):
 
