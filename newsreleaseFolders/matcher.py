d0081e5cb3bc69d3ea6de5d44ddab2cc64c0e820
simonorlovsky <simon.orlovsky@yahoo.com>
2015-01-27 08:47:56 -0600

    Matcher.py added

---
+++ newsreleaseFolders/matcher.py
@@ -0,0 +1,9 @@
+import os, sys
+
+# Open a file
+path = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders"
+dirs = os.listdir( path )
+
+# This would print all the files and directories
+for file in dirs:
+   print file
