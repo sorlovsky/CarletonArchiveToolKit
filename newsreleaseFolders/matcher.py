0075600b3aab3ffa9374bcee1bb3c4c598dc90fc
simonorlovsky <simon.orlovsky@yahoo.com>
2015-01-29 08:42:04 -0600

    Modified

--- newsreleaseFolders/matcher.py
+++ newsreleaseFolders/matcher.py
@@ -1,9 +1,40 @@
 import os, sys
 
 # Open a file
-path = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders"
+path = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories"
 dirs = os.listdir( path )
 
-# This would print all the files and directories
-for file in dirs:
-   print file
+
+#adding all directories to list
+directories = {}
+
+for dir in dirs:
+	if dir[0]!=".":
+		directories[dir] = ''
+
+
+# for i in range(len(directories)):
+# 	newpath = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories/"+directories[i]
+# 	files = os.listdir( path )
+# 	print files
+
+for i, v in enumerate(directories):
+	newpath = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories/"+v
+	files = os.listdir( newpath )
+
+	#Check to make sure this directory isn't empy
+	if files == []:
+		print v, "is empty"
+	directories[v] = files
+	print i, v
+	print "Files:", directories[v]
+	print ""
+
+for i, v in enumerate(directories):
+    os.mkdir(v)
+    src = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories/"
+    dst = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories/"+v
+    shutil.move(src, dst)
+
+
+
