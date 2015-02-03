0e25d44b21cc29c5834e1cd657392f477c007426
simonorlovsky <simon.orlovsky@yahoo.com>
2015-02-03 09:44:17 -0600

    Fixed up news directory

--- newsreleaseFolders/matcher.py
+++ newsreleaseFolders/matcher.py
@@ -1,4 +1,16 @@
-import os, sys
+'''
+matcher.py
+
+Simon Orlovsky
+February 2015
+Carleton College Archives
+
+This is a python script that will a directory filled with folders of pictures that are organized with a particular structure.
+Our goal is to reproduce the file structure of this directory and replace the .jpgs with .tiffs that we have stored on our machine.
+
+'''
+
+import os, sys, shutil
 
 # Open a file
 path = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories"
@@ -10,13 +22,8 @@ directories = {}
 
 for dir in dirs:
 	if dir[0]!=".":
-		directories[dir] = ''
-
-
-# for i in range(len(directories)):
-# 	newpath = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories/"+directories[i]
-# 	files = os.listdir( path )
-# 	print files
+		directories[dir] = '' #the key is the directory name for now it is blank because we haven't put anything into it
+	
 
 for i, v in enumerate(directories):
 	newpath = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories/"+v
@@ -26,15 +33,24 @@ for i, v in enumerate(directories):
 	if files == []:
 		print v, "is empty"
 	directories[v] = files
-	print i, v
+	print "Folder:", v
 	print "Files:", directories[v]
+	filesToCopyOver = directories[v]
+	for i in range(len(filesToCopyOver)):
+		filesToCopyOver[i]= filesToCopyOver[i][:-3]+"tiff"
+	print "Files to move over", filesToCopyOver
+	src = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/tiffs/"
+	print "Source:", src
+	dst = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/"+v
+	print "Destination", dst
 	print ""
 
-for i, v in enumerate(directories):
-    os.mkdir(v)
-    src = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories/"
-    dst = "/Users/simonorlovsky/code/carleton_archive/newsreleaseFolders/directories/"+v
-    shutil.move(src, dst)
+	
+	# os.mkdir(v)
+
+	
+	# shutil.move(src, dst)
+	# print ""
 
 
 
